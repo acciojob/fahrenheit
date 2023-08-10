ba7d1f6bcb8de76c178f33308ff1bd3f16506f34
AYUSHBANSAL-hub <72022069+AYUSHBANSAL-hub@users.noreply.github.com>
2023-08-10 15:03:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+function toFahrenheit(celsius) {
+ // Write your code here
+}
+
+// Do not change the code below
+const celsius = prompt("Enter Celsius:");
+alert(toFahrenheit(Number(celsius)));
